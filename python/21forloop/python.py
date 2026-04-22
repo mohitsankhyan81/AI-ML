e6559ce7f5918b795fd9ddab076a99d8e0b47f90
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-22 21:36:40 +0530

    for loop

---
+++ python/21forloop/python.py
@@ -0,0 +1,13 @@
+# fruit=["apple","banana","mango"]
+# for x in fruit:
+#     print(x)
+
+# for x in "banana":
+#     print(x)
+
+# fruit=["apple","banana","mongo"]
+# for x in fruit:
+#     print(x)
+#     if(x=="banana"):
+#         break
+
