f269a73cf4a94bbfea185c930f737d11d3338e7c
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-22 19:48:10 +0530

    list

---
+++ python/16list/python.py
@@ -0,0 +1,8 @@
+mylist=["mohit","rohit","rohan"]
+print(mylist)
+print(len(mylist))
+print(mylist[0])
+mylist[0]='ankit'
+print(mylist)
+mylist.remove("ankit")
+print(mylist)
---
+++ python/16list/sortlist.py
@@ -0,0 +1,6 @@
+elementlist=[20,45,45,34,23]
+elementlist.sort()
+print(elementlist)
+
+elementlist.reverse()
+print(elementlist)
