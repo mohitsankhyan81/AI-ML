78358325b7ffe29d702b30af56ee3f98fe511de9
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-22 21:48:34 +0530

    function

---
+++ python/22function/per.py
@@ -0,0 +1,17 @@
+#perimiter function
+# def myfunc(fname,lname):
+#     print(fname + " " + lname)
+
+# myfunc("mohit","sankhyan")
+
+#default perimiter
+# def iscoury(country="India"):
+#     print("I am from",country)
+
+# iscoury("Sweden")
+# iscoury("Nepal")
+# iscoury()
+
+def sumfun(a,b):
+    return a+b
+print(sumfun(20,45))
---
+++ python/22function/python.py
@@ -0,0 +1,10 @@
+#print function
+# def statement():
+#     print("My name is mohit sankhyan")
+# statement()
+
+#return fucntion
+def statement():
+#     return "My name is mohit sankhyan i am from kalaka"
+# message=statement()
+# print(message)
