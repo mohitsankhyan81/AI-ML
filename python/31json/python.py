f3fdfd26c500ec5696cc91bcfb117094ba91b16e
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-23 19:39:17 +0530

    json

---
+++ python/31json/python.py
@@ -0,0 +1,18 @@
+import json
+#parse the json data 
+
+# x='{"name":"Mohit","age":45,"class":"10th"}'
+# y=json.loads(x)
+
+# print(y)
+
+#conver data to python to json
+
+# x={
+#     "name":"Mohit",
+#     "age":45,
+#     "class":"10th"
+# }
+
+# y=json.dumps(x)
+# print(y)
