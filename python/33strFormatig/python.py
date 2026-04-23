839bd19f304d472c7d08ea417b071403761a70b2
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-23 20:22:08 +0530

    string formatting

---
+++ python/33strFormatig/python.py
@@ -0,0 +1,10 @@
+# text=f"My name is mohit sankhyan"
+# print(text)
+
+import mypack
+
+price=mypack.data1["price"]
+item=mypack.data1["item"]
+
+data=f"Our Item is: {item} Price is {price}"
+print(data)
