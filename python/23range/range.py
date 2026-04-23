d39cb83b3b79e4a50c884c6ad825ca862efea3d2
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-23 11:38:16 +0530

    range

---
+++ python/23range/range.py
@@ -0,0 +1,21 @@
+# range has three open start stop and step
+
+# x=range(10)
+# print(x)
+# print(list(x))
+
+# x=range(3,10)
+# print(x)
+# print(list(x))
+
+#range using loop
+# for i in range(10):
+#     print(i)
+
+#sepup
+x=range(1,10,2)
+print(x)
+print(list(x))
+
+#set the specific index
+print(x[3])
