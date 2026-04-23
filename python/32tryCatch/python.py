a24482647af2c74c114f37a93b658400ee4d4e07
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-23 19:48:52 +0530

    tryexcept

---
+++ python/32tryCatch/python.py
@@ -0,0 +1,29 @@
+# x=10
+# try:
+#     print(x)
+# except:
+#     print("Exception error")
+
+
+# x=10
+# try:
+#     print(x)
+# except NameError:
+#     print("Varable next is not defined")
+# except:
+#     print("Something another problem")
+
+# try:
+#     print("Hello")
+# except:
+#     print("Something went wrong")
+# else:
+#     print("Nothing wend wrong")
+
+x=10
+try:
+    print(x)
+except:
+    print("Something went wroung")
+finally:
+    print("the Try-execept is finish")
