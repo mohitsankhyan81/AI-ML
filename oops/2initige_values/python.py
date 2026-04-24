9d1befbcd49b2127213eab380b46b6928649ac68
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-24 10:09:58 +0530

    simple python code initalize the values

---
+++ oops/2initige_values/python.py
@@ -0,0 +1,58 @@
+#initalize auto
+
+# class person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+
+# x=str(input("Enter your name: "))
+# y=int(input("Enter you age: "))
+# p1=person(x,y)
+# ans=f"""Hello, {p1.name}
+# Name : {p1.name}
+# Age: {p1.age}"""
+# print(ans)
+
+
+#initalize the data manualy
+# class myclass:
+#     pass
+# p1=myclass()
+# p1.name="mohit"
+# p1.age=45
+# print(p1.name)
+# print(p1.age)
+
+#class initalization
+# class myclass:
+#     def __init__(self,name,aim,age,role):
+#         self.name=name
+#         self.aim=aim
+#         self.age=age
+#         self.role=role
+
+# x=str(input("Enter your Name: "))
+# y=str(input("Enter Your Aim in sort lines: "))
+# z=int(input("Enter you age: "))
+# a=str(input("Enter you role: "))
+
+# p1=myclass(x,y,z,a)
+# print(f"""Hello, Sir
+# MY name is {p1.name}
+# My aim is {p1.aim}
+# My age is {p1.age}
+# MY role is {p1.role}""")
+
+# set default values
+
+# class myclass:
+#     def __init__(self,name,age=18):
+#         self.name=name
+#         self.age=age
+
+# p1=myclass("Mohit")
+# p2=myclass("Sahil",45)
+# print(p1.name)
+# print(p1.age)
+# print(p2.name)
+# print(p2.age)
