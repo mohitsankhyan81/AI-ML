8bde659c9d68f7b195461abbba1839923ce39c9b
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-04-27 14:23:52 +0530

    create file write and append data in file

---
+++ filesystem/WriteCreate/python.py
@@ -0,0 +1,23 @@
+# a is use for append
+# w is use for write
+
+
+#w is overrite your existing content
+# with open("file.txt","w") as f:
+#     print(f.write("""my name is mohit sankhyan and i am from kalka
+#         i am worry for my future
+#      """))
+
+# with open("file.txt") as f:
+#     print(f.read())
+
+#a is append in yor existing data
+# with open("file.txt","a") as f:
+#     f.write("i am doing bca")
+
+# with open("file.txt") as f:
+#     print(f.read())
+
+#x is use for create new file
+f=open("data.txt","x")
+#now file is created 
