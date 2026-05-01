78399fa1b5585ba9bb3e1c3b88fdb3b3603fe8e2
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-05-01 14:45:03 +0530

    shape

---
+++ numpy/7arrayshape/python.py
@@ -0,0 +1,5 @@
+#to get the sape of the array
+
+# import numpy as np
+# arr=np.array([1,2,3,4,5,6])
+# print(arr.shape)
---
+++ numpy/reshape/python.py
@@ -0,0 +1,14 @@
+#1D to 2D
+
+# import numpy as np
+# arr=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# print(arr)
+# newArray=arr.reshape(4,3)
+# print(newArray)
+
+#2D to 3D
+# import numpy as np
+# arr=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# print(arr)
+# newArray=arr.reshape(2,3,2)
+# print(newArray)
