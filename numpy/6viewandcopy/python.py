83fcf764b75d1aab40a2611be796868449ab83e9
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-05-01 12:23:56 +0530

    view and copy

---
+++ numpy/6viewandcopy/python.py
@@ -0,0 +1,23 @@
+#copy the array
+# import numpy as np
+# arr=np.array([1,2,3,4,5])
+# x=arr.copy()
+# arr[0]=34
+# print(arr)
+# print(x)
+
+#view an array
+# import numpy as np
+# arr=np.array([1,2,3,4,5])
+# x=arr.view()
+# arr[0]=34
+# print(arr)
+# print(x)
+
+# import numpy as np
+# arr=np.array([1,2,3,4,5])
+# x=arr.copy()
+# y=arr.view()
+
+# print(x.base)
+# print(y.base)
