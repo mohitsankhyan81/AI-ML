b072841e3989ef9c495d8cb1058f2c872438492c
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-05-05 10:36:06 +0530

    search the data

---
+++ numpy/11searcharray/python.py
@@ -0,0 +1,23 @@
+#search array
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6])
+# x=np.where(arr1==5)
+# print(x)
+
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6])
+# x=np.where(arr1%2==1)
+# print(x)
+
+#binary search is use in numpy like the search sorted
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6,7,8])
+# x=np.serachsorted(arr1,4)
+# print(x) //print the index
+
+
+#search right ! right side se serach karna start kare ga
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6,7,8])
+# x=np.serachsorted(arr1,2,side='left')
+# print(x)
