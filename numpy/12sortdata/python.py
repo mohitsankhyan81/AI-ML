6cf6f27dd3ce693733219dcb14016bd931fc3ae7
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-05-05 10:47:23 +0530

    sort array

---
+++ numpy/12sortdata/python.py
@@ -0,0 +1,10 @@
+#Sort the array
+
+# import numpy as np
+# arr=np.array([3,0,2,5])
+# print(np.sort(arr))
+
+
+# import numpy as np
+# arr=np.array(['banana','data','apple'])
+# print(np.sort(arr))
