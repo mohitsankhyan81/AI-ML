221c17d426aea2f315ac3f967b631bc6f34d220a
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-05-05 10:20:32 +0530

    split array

---
+++ numpy/10arraysplit/python.py
@@ -0,0 +1,28 @@
+#split the array into the parts
+#this is use to split the array into the diffrent parts
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6])
+# newarr=np.array_split(arr,3)
+# print(newarr)
+
+#split the array into the 4 parts
+# import numpy as np 
+# arr1=np.array([1,2,3,4,5,6])
+# newarr=np.array_split(arr,4)
+# print(newarr)
+
+#split the array and access that into the diffrent values
+# import numpy as np
+# arr1=np.array([1,2,3,4,5,6])
+# newarr=np.array_split(arr1,3)
+# print(newarr[0])
+# print(newarr[1])
+# print(newarr[2])
+
+#spliting the 2d array
+# import numpy as np
+# arr1=np.array([[1,2],[3,4],[5,6],[7,8]])
+# newarr=np.array_split(arr1,2)
+# print(newarr)
+# print(newarr[0])
+# print(newarr[1])
